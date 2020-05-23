5b27699346e09972667756351488c10de6f21478
EduardoJM <eduardo_y05@outlook.com>
2020-05-23 11:29:13 -0300

    Updates

---
+++ precache-manifest.1f404feaebba004396051874808d862e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19978dbc20829e01f38151fc35a568b2",
+    "url": "/apollo-gallery/index.html"
+  },
+  {
+    "revision": "44c04980b9a9314aa1b2",
+    "url": "/apollo-gallery/static/js/2.ef029bd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/apollo-gallery/static/js/2.ef029bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4e6a1c3104274be81b",
+    "url": "/apollo-gallery/static/js/main.b2be1e45.chunk.js"
+  },
+  {
+    "revision": "23968ad4742776d00dde",
+    "url": "/apollo-gallery/static/js/runtime-main.d387db5b.js"
+  }
+]);
